74a08a32d5766d898e47621f46b5c169881d18a6
Aleksandar Gargenta <sasa@gargenta.com>
2013-06-04 02:20:18 -0700

    IO Solution (tests missing)

--- src/com/marakana/mydex/cli/Main.java
+++ src/com/marakana/mydex/cli/Main.java
@@ -1,13 +1,17 @@
 package com.marakana.mydex.cli;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
 import com.marakana.mydex.dao.AddressBook;
+import com.marakana.mydex.dao.AddressBookException;
 import com.marakana.mydex.dao.FileBasedAddressBook;
 import com.marakana.mydex.domain.Contact;
 
 public class Main {
+	private static final String USAGE = "Usage: Main <dir>";
+
 	private static final String PROMPT = "address-book> ";
 
 	private static final String HELP = "Usage: quit|help|list|get <email>|delete <email>|store <first-name> <last-name> <email> [phone]";
@@ -20,54 +24,63 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws Exception {
-		// TODO: Do we need to initialize our FileBasedAddressBook???
-		AddressBook addressBook = new FileBasedAddressBook();
+		if (args.length < 1) {
+			System.err.println(USAGE);
+			return;
+		}
+		AddressBook addressBook = new FileBasedAddressBook(new File(args[0]));
 
 		System.out.print(PROMPT);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
 		String line;
 		while ((line = reader.readLine()) != null) {
-			if ("quit".equals(line)) {
-				addressBook.close();
-				break;
-			} else if ("help".equals(line)) {
-				System.out.println("OK");
-				System.out.println(HELP);
-			} else if ("list".equals(line)) {
-				for (Contact contact : addressBook.getAll()) {
-					System.out.println(contact);
-				}
-				System.out.println("OK");
-			} else {
-				String[] request = line.split(" ");
-				if (request.length >= 2) {
-					if ("get".equals(request[0])) {
-						Contact contact = addressBook.getByEmail(request[1]);
-						if (contact == null) {
-							error("No such contact", false);
-						} else {
-							System.out.println(contact);
+			try {
+				if ("quit".equals(line)) {
+					addressBook.close();
+					break;
+				} else if ("help".equals(line)) {
+					System.out.println("OK");
+					System.out.println(HELP);
+				} else if ("list".equals(line)) {
+					for (Contact contact : addressBook.getAll()) {
+						System.out.println(contact);
+					}
+					System.out.println("OK");
+				} else {
+					String[] request = line.split(" ");
+					if (request.length >= 2) {
+						if ("get".equals(request[0])) {
+							Contact contact = addressBook
+									.getByEmail(request[1]);
+							if (contact == null) {
+								error("No such contact", false);
+							} else {
+								System.out.println(contact);
+								System.out.println("OK");
+							}
+						} else if ("delete".equals(request[0])) {
+							addressBook.deleteByEmail(request[1]);
 							System.out.println("OK");
+						} else if ("store".equals(request[0])
+								&& request.length >= 4) {
+							Contact contact = new Contact(request[1],
+									request[2], request[3]);
+							if (request.length >= 5) {
+								contact.setPhone(request[4]);
+							}
+							addressBook.store(contact);
+							System.out.println("OK");
+						} else {
+							error("Invalid request", true);
 						}
-					} else if ("delete".equals(request[0])) {
-						addressBook.deleteByEmail(request[1]);
-						System.out.println("OK");
-					} else if ("store".equals(request[0])
-							&& request.length >= 4) {
-						Contact contact = new Contact(request[1], request[2],
-								request[3]);
-						if (request.length >= 5) {
-							contact.setPhone(request[4]);
-						}
-						addressBook.store(contact);
-						System.out.println("OK");
 					} else {
 						error("Invalid request", true);
 					}
-				} else {
-					error("Invalid request", true);
 				}
+			} catch (AddressBookException e) {
+				System.err.print(e.getMessage());
+				e.printStackTrace();
 			}
 			System.out.print(PROMPT);
 		}
--- src/com/marakana/mydex/dao/FileBasedAddressBook.java
+++ src/com/marakana/mydex/dao/FileBasedAddressBook.java
@@ -1,32 +1,115 @@
 package com.marakana.mydex.dao;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import javax.xml.bind.DatatypeConverter;
+
 import com.marakana.mydex.domain.Contact;
 
-//TODO: implement!
 public class FileBasedAddressBook implements AddressBook {
+
+	private static final String EXTENSION = ".contact";
+
+	private static final FileFilter FILE_FILTER = new FileFilter() {
+		@Override
+		public boolean accept(File pathname) {
+			return pathname.isFile() && pathname.getName().endsWith(EXTENSION);
+		}
+	};
+
+	private final File dir;
+
+	public FileBasedAddressBook(File dir) throws AddressBookException {
+		if (dir == null) {
+			throw new NullPointerException("Dir must not be null");
+		} else if (!dir.exists() && !dir.mkdirs()) {
+			throw new AddressBookException("No such directory: "
+					+ dir.getAbsolutePath());
+		}
+		this.dir = dir;
+	}
+
+	private File getFileForEmail(String email) {
+		if (email == null) {
+			throw new NullPointerException("Email must not be null");
+		} else if (email.isEmpty()) {
+			throw new IllegalArgumentException("Email must not be empty");
+		} else {
+			return new File(this.dir, DatatypeConverter.printHexBinary(email
+					.getBytes()) + EXTENSION);
+		}
+	}
+
+	private Contact getContactForFile(File file) throws AddressBookException {
+		try {
+			try (InputStream in = new FileInputStream(file);
+					ObjectInputStream actualIn = new ObjectInputStream(
+							new GZIPInputStream(in));) {
+				return (Contact) actualIn.readObject();
+			}
+		} catch (IOException | ClassNotFoundException | ClassCastException e) {
+			throw new AddressBookException("Failed to get contact from file ["
+					+ file.getAbsolutePath() + "]", e);
+		}
+	}
 
 	@Override
 	public Contact getByEmail(String email) throws AddressBookException {
-		throw new UnsupportedOperationException("Implement me!");
+		File file = this.getFileForEmail(email);
+		if (file.exists()) {
+			return this.getContactForFile(file);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public Contact[] getAll() throws AddressBookException {
-		throw new UnsupportedOperationException("Implement me!");
+		File[] files = this.dir.listFiles(FILE_FILTER);
+		Contact[] contacts = new Contact[files.length];
+		for (int i = 0; i < contacts.length; i++) {
+			contacts[i] = this.getContactForFile(files[i]);
+		}
+		return contacts;
 	}
 
 	@Override
 	public void store(Contact contact) throws AddressBookException {
-		throw new UnsupportedOperationException("Implement me!");
+		File file = this.getFileForEmail(contact.getEmail());
+		try {
+			try (OutputStream out = new FileOutputStream(file);
+					ObjectOutputStream actualOut = new ObjectOutputStream(
+							new GZIPOutputStream(out));) {
+				actualOut.writeObject(contact);
+			}
+		} catch (IOException e) {
+			file.delete();
+			throw new AddressBookException("Failed to store [" + contact + "]",
+					e);
+		}
 	}
 
 	@Override
 	public void deleteByEmail(String email) throws AddressBookException {
-		throw new UnsupportedOperationException("Implement me!");
+		File file = this.getFileForEmail(email);
+		if (file.exists() && !file.delete()) {
+			throw new AddressBookException(
+					"Failed to delete contact by email [" + email + "]");
+		}
 	}
 
 	@Override
 	public void close() throws AddressBookException {
-		throw new UnsupportedOperationException("Implement me!");
+		// do nothing
 	}
 }

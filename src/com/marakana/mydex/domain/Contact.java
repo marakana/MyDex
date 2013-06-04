2848370de4f11ec937b4aeaa068313d3ad45f209
Aleksandar Gargenta <sasa@gargenta.com>
2013-06-04 01:51:40 -0700

    Solution

--- src/com/marakana/mydex/domain/Contact.java
+++ src/com/marakana/mydex/domain/Contact.java
@@ -10,6 +10,9 @@ public class Contact {
 	private String phone;
 
 	public Contact(String email) {
+		if (email == null) {
+			throw new NullPointerException("Email must not be null");
+		}
 		this.email = email;
 	}
 
@@ -41,9 +44,45 @@ public class Contact {
 		return this.email;
 	}
 
-	// TODO: implement boolean equals(Object)
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((this.email == null) ? 0 : this.email.hashCode());
+		return result;
+	}
 
-	// TODO: implement int hashCode()
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (this.email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!this.email.equals(other.email))
+			return false;
+		return true;
+	}
 
-	// TODO: implement String toString()
+	@Override
+	public String toString() {
+		StringBuilder out = new StringBuilder(35);
+		if (this.getFirstName() != null) {
+			out.append(this.getFirstName()).append(' ');
+		}
+		if (this.getLastName() != null) {
+			out.append(this.getLastName()).append(' ');
+		}
+		out.append('<').append(this.getEmail()).append('>');
+		if (this.getPhone() != null) {
+			out.append(' ').append(this.getPhone());
+		}
+		return out.toString();
+	}
 }

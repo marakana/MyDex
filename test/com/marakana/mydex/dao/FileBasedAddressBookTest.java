d6f7833a1b0d1428d4099dacf8e0c0a5858b494f
Aleksandar Gargenta <sasa@gargenta.com>
2013-06-06 00:55:51 -0700

    Added a note

--- test/com/marakana/mydex/dao/FileBasedAddressBookTest.java
+++ test/com/marakana/mydex/dao/FileBasedAddressBookTest.java
@@ -2,13 +2,14 @@ package com.marakana.mydex.dao;
 
 import java.io.File;
 
+//TODO: it would be better to test contact resolvers and transcoders independently of this class
 public class FileBasedAddressBookTest extends AbstractAddressBookTest {
 	private File dir;
 
 	@Override
 	protected AddressBook buildAddressBook() throws AddressBookException {
-		this.dir = new File(new File(System.getProperty("java.io.tmpdir")),
-				"test-" + System.currentTimeMillis());
+		this.dir = new File(new File(System.getProperty("java.io.tmpdir")), "test-"
+				+ System.currentTimeMillis());
 		this.dir.mkdirs();
 		return new FileBasedAddressBook(this.dir);
 	}
